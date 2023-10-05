23baee15433398fc6b51ac34ff770467cb2a6154
Erica Delagnier <erica@agatha-budget.fr>
2023-10-05 12:04:14 +0200

    setup

--- properties.ts
+++ properties.ts
@@ -1,7 +1,7 @@
 enum Properties {
-    server = 'https://tresorier-back.herokuapp.com/',
-    refreshUrl = 'https://tresorier-back.herokuapp.com/session/refresh',
-    commitHash = '55df010cdf38b139dab95e45a2df9a270a5369a7'
+    server = 'http://localhost:7000/',
+    refreshUrl = 'http://localhost:7000/callback',
+    commitHash = ''
 }
 
 export default Properties
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with pkgs;
 mkShell {
 # seems we should use jdk11_headless for server env
 buildInputs = [
-nodejs-18_x jdk11 git
+nodejs_20 jdk git
 ];
 shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
