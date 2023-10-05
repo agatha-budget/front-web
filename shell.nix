071677d664ca10a05ddfc23f2f5e321aed2ef5a7
Erica Delagnier <erica@agatha-budget.fr>
2023-10-05 12:28:17 +0200

    add keycloak

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with pkgs;
 mkShell {
 # seems we should use jdk11_headless for server env
 buildInputs = [
-nodejs_20 jdk git
+nodejs_18 jdk git
 ];
 shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
