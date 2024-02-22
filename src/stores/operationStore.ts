ae4aebdfde97d407cfb307a547d3126693b05c91
Erica Delagnier <erica.delagnier@multi.coop>
2024-02-22 14:48:57 +0100

    store operation by account Id

--- src/stores/operationStore.ts
+++ src/stores/operationStore.ts
@@ -4,16 +4,16 @@ import { defineStore } from 'pinia'
 
 export const useOperationStore = defineStore('operation', {
   state: () => ({
-    operations: [] as OperationWithDaughters[],
+    operations: {} as Record<string, OperationWithDaughters[]>,
     storeLoaded: false,
   }),
   actions: {
     reset() {
-      this.operations = []
+      this.operations = {}
       this.storeLoaded = false
     },
     retrieveOperations(accounts: Account[]) {
-      this.operations = [] // reset to prevent duplication of operations
+      this.operations = {} // reset to prevent duplication of operations
       Promise.all(accounts.map(
         async (account) => this.retrieveOperationsForAccount(account))
       ).then(
@@ -23,24 +23,18 @@ export const useOperationStore = defineStore('operation', {
     async retrieveOperationsForAccount(account: Account) {
       let res = await OperationService.getOperations(account)
         if (res.isOk()) {
-          this.operations = this.operations?.concat(res.value)
+          this.operations[account.id] = res.value
         } else {
           alert(`Désolée ! un problème est survenu lors du chargement des opérations du compte ${account.name}`)
         }
     },
     getOperationByAccount(accountId: string) : OperationWithDaughters[] {
-      const operations: OperationWithDaughters[] = []
-      for (const op of this.operations) {
-        if (op.accountId === accountId) {
-          operations.push(op)
-        }
-      }
-      return operations 
+      return this.operations[accountId] 
     },
     getOperationByAccountAndCategory(accountId: string, categoryId: string) : OperationWithDaughters[] {
       const operations: OperationWithDaughters[] = []
-      for (const op of this.operations) {
-        if (op.accountId === accountId && op.categoryId === categoryId) {
+      for (const op of this.operations[accountId]) {
+        if (op.categoryId === categoryId) {
           operations.push(op)
         }
       }
