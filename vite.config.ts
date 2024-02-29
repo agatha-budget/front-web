0ed943857ff1f124909697ae8fabd2de3c07bc60
Erica Delagnier <erica.delagnier@multi.coop>
2024-02-29 16:54:02 +0100

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
