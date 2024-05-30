61738f03ad675f6d693d22506a19acc5d6ce492a
edelagnier <erica.delagnier@multi.coop>
2024-05-30 17:25:09 +0200

    update loader

--- src/components/utils/Loader.vue
+++ src/components/utils/Loader.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="loader-container">
+    <h3 class="loaderTitle" v-if="message">{{message}}</h3>
+    <h3 class="loaderTitle" v-else="message">{{ $t("DEFAULT_LOADING_MSG")}}</h3>
     <div class="image-spinner">
       <img src="@/assets/logo_round.png"/>
       <div class="spin spinner"></div>
@@ -11,6 +13,12 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'LoaderCmpt'
+  name: 'LoaderCmpt',
+  props: {
+    message: {
+      type: String,
+      required: false
+    }
+  },
 })
 </script>
